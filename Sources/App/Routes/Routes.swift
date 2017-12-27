7adb99bdff4f91f97d05964ed41cc4094e5ed4fc
Aleix Baggerman <aleixdiazbaggerman@gmail.com>
2017-12-27 19:17:27 +0100

    [CORE] main.leaf forms added

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -11,6 +11,10 @@ extension Droplet {
         get("plaintext") { req in
             return "Hello, world!"
         }
+        
+        get("chat") {req in
+            return try self.view.make ("main.leaf")
+        }
 
         // response to requests to /info domain
         // with a description of the request
