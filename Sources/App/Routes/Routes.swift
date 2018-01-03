f104851167b3ca1acdf49b17d5eb102ef7a7580e
Aleix Baggerman <aleixdiazbaggerman@gmail.com>
2018-01-03 21:19:16 +0100

    Chat friend and self working

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -2,91 +2,60 @@ import Vapor
 
 extension Droplet {
     func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { request in
-
-            return "si"
-        }
-        
-        post ("user") { req in
-            guard let text = req.data["messageText"]?.string else {
-                throw Abort(.badRequest)
-            }
-            
-            let message = Message (userName: "Berni", messageText: text)
-            DataManager.messages.append (message)
-            var ttext = ""
-            for message in DataManager.messages {
-                ttext += message.messageText
-            }
-            
-            return ttext
-        }
-        
         post ("chat") { req in
-//            //Guardem el missatge al array
-//            if let text = req.data["messageText"]?.string {
-//                let message = Message (userName: "Berni", messageText: text)
-//                DataManager.messages.append (message)
-//            }
             
-            //El printem per pantalla amb leaf
-            var messages = [Node] ()
             var isUserSaved = true
+            var currentUserName: String?
+            
+            //Si es el primer cop que l'usuari entra al chat s'haura de guardar el seu nom d'usuari
             if let userName = req.data["userName"]?.string {
+                currentUserName = userName
                 isUserSaved = false
             }
             
             if isUserSaved {
+                //Si el nom ja esta guardat entra aqui...
+                
                 let session = try req.assertSession()
                 
+                //Agafem el nom guardat a la sessio de l'usuari.
                 guard let userName = session.data["userName"]?.string else {
                     return "error fetching user"
                 }
-                
-                //Guardem el missatge al array
+                currentUserName = userName
+                //Si existeix un missatge, el guardem a DataManager
                 if let text = req.data["messageText"]?.string {
                     let message = Message (userName: userName, messageText: text)
                     DataManager.messages.append (message)
                 }
-                
-                for message in DataManager.messages {
-                    let messageAux = Message (userName: message.userName, messageText: message.messageText)
-                    messages.append (try messageAux.makeNode (context: nil))
-                }
             } else {
+                //Si el nom no estava guardat entra aqui...
+                //Creem una nova sessio i guardem el usuari que rebem per la request.
                 guard let userName = req.data["userName"]?.string else {return "Error getting user from userAuth"}
                 let session = try req.assertSession()
                 try session.data.set("userName", userName)
             }
             
+            //Convertim a tipus Node tots els missatges de DataManager
+            var messages = [Node] ()
+            for message in DataManager.messages {
+                let messageAux = Message (userName: message.userName, messageText: message.messageText)
+                if messageAux.userName == (currentUserName ?? "") {
+                    messageAux.owner = "owner"
+                } else {
+                    messageAux.owner = "notOwner"
+                }
+                print (messageAux.owner, messageAux.messageText)
+                messages.append (try messageAux.makeNode (context: nil))
+            }
+            
             return try self.view.make ("main.leaf", Node (node: ["messages":messages]))
         }
         
-        get("chat") {req in
-            var messages = [String] ()
-            for message in DataManager.messages {
-                messages.append (message.messageText)
-            }
-            return try self.view.make ("main.leaf", ["messages":messages])
-        }
         
         get ("userAuth") { req in
             return try self.view.make ("userAuth.leaf")
         }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
         
         try resource("posts", PostController.self)
     }
